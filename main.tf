807dfc11b47e11cb0e7399c343fcc06d02db3877
Junior Lafuente <valterlafuentejunior@gmail.com>
2023-07-10 16:00:53 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f65b8614a860c29"
+  key_name      = "iac-lafuente"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instancia-lafuente"
+  }
+}
