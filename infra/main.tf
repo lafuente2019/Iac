e989405970500a36bee5614a53d5a26e5b53c565
Junior Lafuente <valterlafuentejunior@gmail.com>
2023-07-11 18:44:50 -0300

    configurando loadBalancer

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,8 @@ resource "aws_launch_template" "maquina" {
   tags = {
     Name = "Terraform Ansible python"
   }
-  security_group_names = [var.var.grupoDeSeguranca]
+  security_group_names = [var.grupoDeSeguranca]
+  user_data = filebase64("ansible.sh")
 }
 
 resource "aws_key_pair" "chaveSSH" {
@@ -28,16 +29,42 @@ resource "aws_key_pair" "chaveSSH" {
   public_key = file("${var.chave}.pub")
 }
 
-output "IP_publico" {
-  value = aws_instance.app_server.public_ip
-}
+#output "IP_publico" {
+#  value = aws_instance.app_server.public_ip
+#}
 
 resource "aws_autoscaling_group" "grupo" {
+  availability_zones = [ "${var.regiao_aws}a","${var.regiao_aws}b" ]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
   launch_template {
       id = aws_launch_template.maquina.id
       version = "$Latest"
-    }
+  }
+  target_group_arns  = [aws_lb_target_group.alvoLoadBalancer.arns]
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a"
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b"
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets = [aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id]
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+  
+}
+
+resource "aws_default_vpc" "default" {
 }
